70c9da9a003239959360f8e18e248f4e39331ab6
Ashutosh Kumar Singh <43166870+ashudnsingh@users.noreply.github.com>
2018-11-06 15:35:58 +0530

    Create 034 - countBlackCells.py

---
+++ Loop Tunnel/034 - countBlackCells.py
@@ -0,0 +1,10 @@
+def find_gcd(a, b):
+    while b != 0:
+        a, b = b, a % b
+    return a
+
+def countBlackCells(n, m):
+    gcd = find_gcd(n, m)
+    line_cells = n + m - gcd
+    line_corner_cells = (gcd - 1) * 2
+    return line_cells + line_corner_cells
