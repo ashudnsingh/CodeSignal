d89dbc88da6982357b647d93174a3153ffc222fb
Ashutosh Kumar Singh <43166870+ashudnsingh@users.noreply.github.com>
2018-11-06 14:08:56 +0530

    Create 059 - spiralNumbers.py

---
+++ Intro/Land of Logic/059 - spiralNumbers.py
@@ -0,0 +1,13 @@
+def spiralNumbers(n):
+    dat = [[None] * n for i in range(n)]
+    le = [[i + 1, i + 1] for i in reversed(range(n))]
+    le = sum(le, [])[1:]  # for n = 5 le will be [5, 4, 4, 3, 3, 2, 2, 1, 1]
+    dxdy = [[1, 0], [0, 1], [-1, 0], [0, -1]] * int((len(le) + 4) / 4)  # long enough
+    x, y, val = -1, 0, 0
+    for steps, (dx, dy) in zip(le, dxdy):
+        x, y, val = x + dx, y + dy, val + 1
+        for j in range(steps):
+            dat[y][x] = val
+            if j != steps-1:
+                x, y, val = x + dx, y + dy, val + 1
+    return dat
