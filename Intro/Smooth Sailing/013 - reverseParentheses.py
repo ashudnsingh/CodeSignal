b97bcd289dbbef18b440eaba1f913316b769c820
Ashutosh Kumar Singh <43166870+ashudnsingh@users.noreply.github.com>
2018-11-06 13:09:24 +0530

    Create 013 - reverseParentheses.py

---
+++ Intro/Smooth Sailing/013 - reverseParentheses.py
@@ -0,0 +1,8 @@
+def reverseParentheses(s):
+    for i in range(len(s)):
+        if s[i] == "(":
+            start = i
+        if s[i] == ")":
+            end = i
+            return reverseParentheses(s[:start] + s[start+1:end][::-1] + s[end+1:])
+    return s
