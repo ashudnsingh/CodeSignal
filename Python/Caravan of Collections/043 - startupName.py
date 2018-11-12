6dcd045964e3aff7a5e60d246ffd40af6c49a244
Ashutosh Kumar Singh <43166870+ashudnsingh@users.noreply.github.com>
2018-11-12 16:57:32 +0530

    Create 043 - startupName.py

---
+++ Python/Caravan of Collections/043 - startupName.py
@@ -0,0 +1,6 @@
+def startupName(companies):
+    cmp1 = set(companies[0])
+    cmp2 = set(companies[1])
+    cmp3 = set(companies[2])
+    res = ( ( (cmp1 | cmp2 ) - cmp3 ) | ( (cmp2 | cmp3 ) - cmp1 ) | ( (cmp1 | cmp3 ) - cmp2 ) ) - ( ( cmp1 - (cmp2 | cmp3) ) | ( cmp2 - (cmp1 | cmp3) ) | ( cmp3 - (cmp2 | cmp1) ) )
+    return list(sorted(list(res)))
