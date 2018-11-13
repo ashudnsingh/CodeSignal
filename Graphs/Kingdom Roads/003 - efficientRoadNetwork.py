7f6b78d9965d7689147607df2298ad425a9e058c
Ashutosh Kumar Singh <43166870+ashudnsingh@users.noreply.github.com>
2018-11-13 11:45:44 +0530

    Update 003 - efficientRoadNetwork.py

--- Graphs/Kingdom Roads/003 - efficientRoadNetwork.py
+++ Graphs/Kingdom Roads/003 - efficientRoadNetwork.py
@@ -1,1 +1,11 @@
-
+def efficientRoadNetwork(n, roads):
+    adj = [[] for i in range(n)]
+    for rd in roads:
+        adj[rd[0]].append(rd[1])
+        adj[rd[1]].append(rd[0])
+    for city in range(n - 1):
+        oneHop = {c for c in adj[city]}
+        twoHops = {c for c1 in oneHop for c in adj[c1]}    
+        if len({city} | oneHop | twoHops) < n:
+            return False
+    return True
